88b7dbab05b1eae77f576e2e2013b937e6f0389a
miguelraatz <miguelraatz_@hotmail.com>
2023-07-17 15:31:01 -0300

    feat: implemented function process for requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines_data = txt_importer(path_file)
+    new_instance = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines_data),
+        "linhas_do_arquivo": lines_data,
+    }
+    if new_instance in instance._data:
+        return None
+    instance.enqueue(new_instance)
+    print(new_instance)
 
 
 def remove(instance):

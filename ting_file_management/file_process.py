4aa0bd866f2bbf870ddce0fd4bf44295679b1f3d
miguelraatz <miguelraatz_@hotmail.com>
2023-07-17 15:55:36 -0300

    feat: implemented function file_metadata for requirement 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
@@ -24,4 +25,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        search_queue = instance.search(position)
+        print(search_queue)
+    except IndexError:
+        print('Posição inválida', file=sys.stderr)

3516774bb790e61af229a13b2053db09e634b92b
miguelraatz <miguelraatz_@hotmail.com>
2023-07-17 13:11:19 -0300

    feat: implemented class queue for requirement 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,26 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+        return self._data.pop(0)
+
+    def size(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return not self.size()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if self.is_empty() or index > self.size() - 1 or index < 0:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._data[index]
